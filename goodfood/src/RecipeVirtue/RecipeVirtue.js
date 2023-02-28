d0c9b2214b1833591e0b411120dadc092a8c6e1e
GaelCantonnet <gcantonnet@gmail.com>
2023-02-28 17:01:51 +0100

    debut des filtres de recette

--- goodfood/src/RecipeVirtue/RecipeVirtue.js
+++ goodfood/src/RecipeVirtue/RecipeVirtue.js
@@ -5,8 +5,7 @@ import { useMediaQuery } from 'react-responsive';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Loader from '../Loader/Loader';
-import {useParams} from "react-router-dom";
-import RecipesCards from '../RecipesCards/RecipesCards';
+
 
 
 
@@ -14,10 +13,12 @@ function Virtue(){
     const {virtuename}=useParams();
     const {recipes} = useRecipesContext(); 
     console.log(recipes)
+    console.log()
     
-    /*const recipesToShow = recipes.filter(recipe => recipe.virtue = virtuename)*/
 
-    
+    const filteredRecipes = recipes.filter(recipe => recipe.virtue === {virtuename})
+
+    console.log(filteredRecipes)
 
       return (
         <div>
