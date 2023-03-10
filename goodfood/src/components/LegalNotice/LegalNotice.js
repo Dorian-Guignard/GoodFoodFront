ec941449a0a457f467009b6f871915d98d425f98
GaelCantonnet <gcantonnet@gmail.com>
2023-03-10 11:56:51 +0100

    legalnoticeupdate

--- goodfood/src/components/LegalNotice/LegalNotice.js
+++ goodfood/src/components/LegalNotice/LegalNotice.js
@@ -5,7 +5,31 @@ function LegalNotice(){
     return(
     <div className="legalNotice">
         <Title>Mention Légales</Title>
-        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue.</div>
+        <div>
+
+Editeur du site :
+
+GoodFood <br/>
+
+Propriété intellectuelle :<br/>
+
+Tous les éléments (textes, images, vidéos, etc.) présents sur le site sont protégés par le droit d'auteur et la propriété intellectuelle. Toute reproduction, même partielle, est interdite sans autorisation préalable.<br/>
+
+Responsabilité :<br/>
+
+L'éditeur ne peut être tenu responsable des dommages directs ou indirects résultant de l'utilisation du site web, notamment en cas d'interruption de service, de virus informatique ou de perte de données.<br/>
+
+Données personnelles :<br/>
+
+Les données personnelles collectées sur le site (nom, prénom, adresse email, etc.) sont utilisées dans le respect de la réglementation en vigueur (RGPD) et ne sont pas transmises à des tiers sans autorisation préalable.<br/>
+
+Cookies :<br/>
+
+Le site utilise des cookies pour améliorer l'expérience utilisateur et proposer des contenus adaptés aux préférences de l'utilisateur. Il est possible de refuser les cookies en configurant son navigateur internet.<br/>
+
+Loi applicable et juridiction compétente :<br/>
+
+Le site est soumis à la loi française. Tout litige relatif au site sera de la compétence exclusive des tribunaux français.</div>
         </div>
     )
 }
